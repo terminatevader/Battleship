5edbf2f226b497213a1a2af3c81e0bdcc11fba48
TermiNateVader <nathanbenstevens@gmail.com>
2024-12-23 21:43:58 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,91 +2,93 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <Windows.h>
-#include <string>
+#include <cstdlib>
+//#include <string>
 
 using namespace std;
 
 int main()
 {
-	string P1B1;
-	cout << "Player 1 enter the cooridinates for battleship 1 \n";
-	getline(cin, P1B1);
-	string P1B2;
-	cout << "Player 1 enter the cooridinates for battleship 2 \n";
-	getline(cin, P1B2);
-	string P1B3;
-	cout << "Player 1 enter the cooridinates for battleship 3 \n";
-	getline(cin, P1B3);
-	
-	string P2B1;
-	cout << "Player 2 enter the cooridinates for battleship 1 \n";
-	getline(cin, P2B1);
-	string P2B2;
-	cout << "Player 2 enter the cooridinates for battleship 2 \n";
-	getline(cin, P2B2);
-	string P2B3;
-	cout << "Player 2 enter the cooridinates for battleship 3 \n";
-	getline(cin, P2B3);
+	int P1B1S = 0;
+	int P1B2S = 0;
+	int P1B3S = 0;
+	int P2B1S = 0;
+	int P2B2S = 0;
+	int P2B3S = 0;
+
+	cout << "Welcome to the Battleship pre-release. Pick a number between 1 and 10 when prompted for cooridinates. \n";
+	Sleep(1000);
+	int P1B1;
+	cout << "Enter the cooridinates for battleship 1 \n";
+	cin >> P1B1;
+	int P1B2;
+	cout << "Enter the cooridinates for battleship 2 \n";
+	cin >> P1B2;
+	int P1B3;
+	cout << "Enter the cooridinates for battleship 3 \n";
+	cin >> P1B3;
 
 
-	while (( (P1B1 != "sunk") || (P1B2 != "sunk") || (P1B3 != "sunk")) && ((P2B1 != "sunk") || (P2B2 != "sunk") || (P2B3 != "sunk")))
+	cout << "Computer is entering the cooridinates for battleship 1 \n";
+	Sleep(5000);
+	int P2B1 = 1 + rand() % 11;
+	cout << "Computer is entering the cooridinates for battleship 2 \n";
+	Sleep(5000);
+	int P2B2 = 1 + rand() % 11;
+	cout << "Computer is entering the cooridinates for battleship 3 \n";
+	Sleep(5000);
+	int P2B3 = 1 + rand() % 11;
+
+
+	while (P1B1S == 0 && P1B2S == 0 && P1B3S == 0 || P2B1S == 0 && P2B2S == 0 && P2B3S == 0)
 	{
-		
-		string p1;
-		cout << "Player 1 turn \n";
-		getline(cin, p1);
-		if (p1 == P2B1)
+		int P1;
+		cout << "It is your turn \n";
+		cin >> P1;
+
+		if (P1 == P2B1)
 		{
-			cout << "Player 2 battleship has been sunk! \n";
-			P2B1 = "sunk";
+			cout << "You sunk computer's battleship \n";
+			P2B1S = 1;
 		}
-		else if (p1 == P2B2)
+		if (P1 == P2B2)
 		{
-			cout << "Player 2 battleship has been sunk! \n";
-			P2B1 = "sunk";
+			cout << "You sunk computer's battleship \n";
+			P2B2S = 1;
 		}
-		else if (p1 == P2B3)
+		if (P1 == P2B3)
 		{
-			cout << "Player 2 battleship has been sunk! \n";
-			P2B1 = "sunk";
+			cout << "You sunk computer's battleship \n";
+			P2B3S = 1;
 		}
 
-		else
+		int P2;
+		cout << "It is computer's turn \n";
+		Sleep(5000);
+		P2 = 1 + rand() % 11;
+
+		if (P2 == P1B1)
 		{
-			cout << "Player 2 battleship has been missed \n";
+			cout << "Computer sunk your battleship \n";
+			P1B1S = 1;
 		}
-		
-		
-		string p2;
-		cout << "Player 2 turn \n";
-		getline(cin, p2);
-		if (p2 == P1B1)
+		if (P2 == P1B2)
 		{
-			cout << "Player 1 battleship has been sunk! \n";
-			P1B1 = "sunk";
+			cout << "Computer sunk your battleship \n";
+			P1B2S = 1;
 		}
-		else if (p2 == P1B2)
+		if (P2 == P1B3)
 		{
-			cout << "Player 1 battleship has been sunk! \n";
-			P1B2 = "sunk";
+			cout << "Computer sunk your battleship \n";
+			P1B3S = 1;
 		}
-		else if (p2 == P1B3)
-		{
-			cout << "Player 1 battleship has been sunk! \n";
-			P1B3 = "sunk"; 
-		}
-		else
-		{
-			cout << "Player 1 battleship has been missed \n";
-		}
-		
-}
-	if ((P1B1 == "sunk") && (P1B2 == "sunk") && (P1B2 == "sunk"))
-	{
-		cout << "Player 2 wins!";
 	}
-	if ((P2B1 == "sunk") && (P2B2 == "sunk") && (P2B2 == "sunk"))
+	if (P1B1S == 1 && P1B2S == 1 && P1B3S == 1)
 	{
-		cout << "Player 1 Wins!";
+		cout << "Computer wins \n";
+	}
+	if (P2B1S == 1 && P2B2S == 1 && P2B3S == 1)
+	{
+		cout << "You win \n";
 	}
 }
